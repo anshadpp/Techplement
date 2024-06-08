1ed7b584fee15317e8eb17dc710a685a566e2e26
shahanajasi <alikallingal456@gmail.com>
2024-06-08 15:17:27 +0530

    data model

--- quotes/urls.py
+++ quotes/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
 
     path('',views.index),
+    path('search/', views.search_quotes, name='search'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from . models import Quotes
-
+import random
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    all_quotes = Quotes.objects.all()
 
 
-def search(request):
-    query=request.GET['search']
-    author = Quotes.objects.filter(Q(place__icontains=query))
-    return render(request,"search.html",locals())
+    random_quote = random.choice(all_quotes)
+    return render(request,'index.html',{'quote': random_quote})
+
+# def quotes(request):
+#     quotes=Quotes.objects.all()
+#
+#     return render(request)
+
+def search_quotes(request):
+    if 'author' in request.GET:
+        author_name = request.GET['author']
+        # Perform case-insensitive search for quotes by author name
+        quotes = Quotes.objects.filter(author__icontains=author_name)
+    else:
+        quotes = []
+
+    return render(request, 'search.html', {'quotes': quotes})
